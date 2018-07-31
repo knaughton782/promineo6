a5d9ec309874df3cdeada3c2ac0f032f27bf4718
Kirsten Naughton <kirsten.naughton@gmail.com>
2018-07-30 17:22:36 -0700

    research

--- src/weekSixHomework/Research.java
+++ src/weekSixHomework/Research.java
@@ -8,13 +8,19 @@ public class Research {
 		 * 
 		 * 		An interface is like a contract and have to be used in the way they are written.
 		 * 		An interface also only has the signature of the methods and can't do anything; its
-		 * 		like a pattern.
+		 * 		like a pattern, not a class.
 		 * 
-		 * 		An Abstract Class 
+		 * 		An Abstract Class cannot be instantiated...meaning you can't make new instances. The point
+		 * 		is to function as a base for subclasses. To use them you have to use the extends keyword.
+		 * 		You can define the behavior for them unlike interfaces.
 		 * 
 		 * Research and write about why Unit Testing is important and what benefits it adds.
 		 * 		
-		 * 		Easier and safer to refactor code. 
+		 * 		A unit is usually the smallest implementation of some code so it is easier to design (your code becomes
+		 * 		more cohesive as you are forced to use each class in testing), execute, and analyze.
+		 * 		Unit testing makes for easier and safer ways to refactor code. Some of the benefits for
+		 * 		using unit testing is to make the process more agile, improve the quality of the code, and allow
+		 * 		for finding the bugs earlier. Unit tests are like guidelines to keep your code on track.
 		 * 
 		 * 
 		 */
